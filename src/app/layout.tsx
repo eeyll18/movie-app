83185b0ceb1c30a710185e3e22ec42408f1e498b
eeyll18 <edaeylulgunay2@gmail.com>
2025-06-07 17:49:52 +0300

    changed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from './(components)/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'My Movie App',
+  title: 'Movie App',
   description: 'A simple app to log your favorite movies with Next.js and Tailwind CSS',
 };
 
